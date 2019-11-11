c2075a0e6c97452ed41b77a2feb830c61efaab5e
Mr Mario Savarese <msavar03@dcs.bbk.ac.uk>
2019-11-11 21:32:40 +0000

    defined distance() and compute_total_distance()

--- cities.py
+++ cities.py
@@ -1,12 +1,17 @@
+import random
+import math
+
 def read_cities(file_name):
 
-    read = open(file_name, "r")
+    parse_cities = open(file_name, "r")
     all_cities = []
 
-    for element in read.readlines():
+    for element in parse_cities.readlines():
         sub = element.rstrip("\n").split("\t")
         state,city,latitude,longitude = sub[0],sub[1],float(sub[2]),float(sub[3])
         all_cities.append((state,city,latitude,longitude))
+
+    parse_cities.close()
 
     return all_cities
     """
@@ -23,15 +28,31 @@ def read_cities(file_name):
 def print_cities(road_map):
     for city in road_map:
         print(city[1]+",",
-              city[0]+",",
+              city[0]+":",
               round(city[2],2),
               round(city[3],2))
     """
     Prints a list of cities, along with their locations. 
     Print only one or two digits after the decimal point.
     """
+def distance(x1,y1,x2,y2):
+
+    return math.sqrt(abs(x1-x2)**2 + abs(y1-y2)**2)
 
 def compute_total_distance(road_map):
+
+    last = len(road_map)-1
+    total_distance = 0
+
+
+    for city in range(len(road_map)):
+        if city != last:
+            total_distance += distance(road_map[city][2],road_map[city][3],road_map[city+1][2],road_map[city+1][3])
+        else:
+            total_distance += distance(road_map[city][2],road_map[city][3],road_map[0][2],road_map[0][3])
+
+    return total_distance
+
 
     """
     Returns, as a floating point number, the sum of the distances of all 
@@ -86,4 +107,4 @@ def main():
 if __name__ == "__main__": #keep this in
     main()
 
-print_cities(read_cities("city-data.txt"))
+    print(compute_total_distance(read_cities("city-data.txt")))
